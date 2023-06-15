79111663019bce918401557a6c698f4ea55489e8
Listus97 <namurendecallistus@gmail.com>
2023-06-15 15:53:55 -0700

    delete dnodeint

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,33 +10,33 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current;
+	dlistint_t *node_b4_del = *head, *node_to_del, *node_af_del;
+	unsigned int i = 1;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
 
-	current = *head;
-
-	if (current == NULL)
-		return (-1);
-
-	while (index != 0)
+	if (index == 0)
 	{
-		current = current->next;
-		index--;
-		if (current == NULL)
+		*head = (*head)->next;
+		(*head)->prev = NULL;
+		free(node_b4_del);
+		return (1);
+	}
+	while (i < index)
+	{
+		if (node_b4_del == NULL)
 			return (-1);
+		node_b4_del = node_b4_del->next;
+		i += 1;
 	}
-
-	if (current->prev != NULL)
-		current->prev->next = current->next;
-	else
-	{
-		*head = current->next;
-		if (current->next != NULL)
-			current->next->prev = NULL;
-		free(current);
-	}
-
+	if (node_b4_del == NULL || node_b4_del->next == NULL)
+		return (-1);
+	node_to_del = node_b4_del->next;
+	node_af_del = node_to_del->next;
+	if (node_af_del != NULL)
+		node_af_del->prev = node_b4_del;
+	node_b4_del->next = node_af_del;
+	free(node_to_del);
 	return (1);
 }
